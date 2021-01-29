79186dd8465d20f7ec7e305f9315dab21efc27f9
david <david@venturplex.com>
2021-01-28 20:49:08 -0700

    create a simple counter app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+function Button(props) {
+  return (
+    <div>
+      <button onClick={props.incrementCounter}>+1</button>
+      <button onClick={props.decrementCounter}>-1</button>
+    </div>
+  );
+}
+
+function Display(props) {
+  return <div>{props.clicks}</div>;
+}
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const increment = () => setCounter(counter + 1);
+  const decrement = () => setCounter(counter - 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Display clicks={counter} />
+      <Button incrementCounter={increment} decrementCounter={decrement} />
     </div>
   );
 }
